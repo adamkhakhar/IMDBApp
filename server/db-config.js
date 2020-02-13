2b99dc2bbb3a91a1eef0fc38e9b268634cae5307
Adam Khakhar <adamkhakhar@huntsman-ve703-06429.apn.wlan.private.upenn.edu>
2020-02-13 18:20:51 -0500

    First commit

--- server/db-config.js
+++ server/db-config.js
@@ -1,7 +1,7 @@
 // Private. Will not be included in submission
 module.exports = {
-  host: "fling.seas.upenn.edu",
-  user: "akhakhar",
-  password: "Snake$321",
-  database: "akhakhar"
+  host: "*************",
+  user: "***********",
+  password: "*************",
+  database: "*************"
 };
